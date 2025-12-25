987bbe7fcfb48c0170aeea0cef9b98f5b98b14eb
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-26 00:31:13 +0530

    Lecture 5 done

---
+++ 5-Tyep conversion/index.js
@@ -0,0 +1,46 @@
+// let age=window.prompt("How old are you?");
+
+// age+=1; // Here age is string so it concatenates
+
+// console.log(age, typeof age);
+
+// age=Number(age);
+// age+=2;
+// console.log(age, typeof age);
+
+
+let x='pizza';
+let y='pizza';
+let z='pizza';
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
+
+x='0';
+y='0';
+z='0';
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
+
+x='';
+y='';
+z='';
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
