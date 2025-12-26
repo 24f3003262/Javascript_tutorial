71bb8ebdd16a2fa45d831d68e4282f73006207fd
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-26 20:17:43 +0530

    add lec 8

---
+++ 8-Math Object/index.js
@@ -0,0 +1,21 @@
+// Math - built in object that provides collection of properties and methods
+
+let x=3.99;
+let y=-2;
+let z;
+
+// z=Math.round(x);
+// z=Math.floor(x);
+// z=Math.ceil(x);
+// z=Math.trunc(x);
+// z=Math.pow(x,y);
+// z=Math.sqrt(x);
+// z=Math.log(y); //Takes log to the base 10
+// z=Math.sin(x);
+// z=Math.atan(y);
+// z=Math.abs(y);
+// z=Math.sign(x); //signum 
+
+// z=Math.max(x,y); //as many var as you want
+
+document.getElementById("myText").textContent=`Output: ${z}`;
