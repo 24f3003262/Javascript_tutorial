00048c3282d30454bf804755d74f084e4d52ffbb
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-27 23:15:41 +0530

    add lec 13

---
+++ 13-Switches/index.js
@@ -0,0 +1,29 @@
+let testScore=80;
+let letterGrade;
+
+switch(true){
+    case testScore>=90:
+        letterGrade="A";
+        break;
+    
+    case testScore>=80:
+        letterGrade="B";
+        break;
+    
+    case testScore>=70:
+        letterGrade="C";
+        break;
+    
+    case testScore>=60:
+        letterGrade="D";
+        break;
+    
+    case testScore>=50:
+        letterGrade="E";
+        break;
+    
+    default:
+        letterGrade="FAIL";
+}
+
+document.getElementById("myText").textContent=letterGrade;
