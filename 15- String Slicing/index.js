8a0e328481ad9568b3f73851adae588210203d8f
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-29 01:10:42 +0530

    add lec 15

---
+++ 15- String Slicing/index.js
@@ -0,0 +1,20 @@
+const fullname="Suby Duby";
+
+// let firstname=fullname.slice(0,4);
+// let lastname=fullname.slice(5,);
+
+// let firstchar=fullname.slice(0,1);
+
+// let lastchars=fullname.slice(-1);
+
+// let firstname=fullname.slice(0,fullname.indexOf(" "));
+// let lastname=fullname.slice(fullname.indexOf(" ")+1);
+// console.log(firstname);
+// console.log(lastname);
+// console.log(firstchar);
+// console.log(lastchars);
+
+
+const email="modna@gmail.com";
+let username=email.slice(0,email.indexOf("@"));
+console.log(username);
