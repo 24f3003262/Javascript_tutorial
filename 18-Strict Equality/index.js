0a0a8aefb67e2b82b8b0369f66c1882c306852d3
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-29 02:57:45 +0530

    add lec 18

---
+++ 18-Strict Equality/index.js
@@ -0,0 +1,18 @@
+// == comparison operator which checks if values are equal
+// === strict equality operator whcih checks if  values and datatypes are equal
+// !== strict inequality operator (if either datatype or value different then it is different)
+
+const PI=3.14;
+
+if(PI=="3.14"){
+    console.log("pi");
+}
+else{
+    console.log("not pi");
+}
+if(PI==="3.14"){
+    console.log("pi");
+}
+else{
+    console.log("not pi");
+}
