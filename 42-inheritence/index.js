ed5b2e0c45a23d6d5f37881517ddb1a4644b0351
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2026-02-10 00:09:38 +0530

    add lec 42

---
+++ 42-inheritence/index.js
@@ -0,0 +1,51 @@
+//inheritence = allows a new class to inherit properties and methods from an existing class (parent->child). It helps with code reusability
+
+class Animal{
+    alive=true;
+    eat(){
+        console.log(`This ${this.name} is eating`);
+    }
+    sleep(){
+        console.log(`${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal{
+    name="rabbit";
+    run(){
+        console.log(`${this.name} is running`);
+    }
+}
+
+class Fish extends Animal{
+    name="fish";
+    swim(){
+        console.log(`${this.name} is swimming`);
+    }
+}
+
+class Hawk extends Animal{
+    name="hawk";
+    fly(){
+        console.log(`${this.name} is flying`);
+    }
+}
+
+const rabbit=new Rabbit();
+const fish=new Fish();
+const hawk=new Hawk();
+
+console.log(rabbit.alive);
+rabbit.alive=false;
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+fish.eat();
+hawk.sleep();
+
+rabbit.run();
+// fish.run(); // Won't work
+fish.swim();
+hawk.fly();
+// hawk.swim(); // Won't work
+// rabbit.fly(); // Won't work
