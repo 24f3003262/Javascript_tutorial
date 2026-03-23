0ae83fbd248f0467a009f9043a70abb295eeeda9
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2026-03-24 02:32:25 +0530

    add lec 70

---
+++ 70 - Callback Hell/index.js
@@ -0,0 +1,50 @@
+// Callback Hell = Situation in JS where callbacks are nested within other callbacks to the degree it becomes difficult to read
+// Old pattern to handle asynchronous functions
+// Use promises + async/await to avoid
+
+function task1(callback){
+
+    setTimeout(()=>{
+    console.log("Task 1 Complete");
+    callback();    
+    },2000);
+
+}
+function task2(callback){
+    setTimeout(()=>{
+        console.log("Task 2 Complete");
+        callback();
+    },1000);
+
+}
+function task3(callback){
+    setTimeout(()=>{
+        console.log("Task 3 Complete");
+        callback();
+    },3000);
+}
+function task4(callback){
+    setTimeout(()=>{
+        console.log("Task 4 Complete");
+        callback();
+    },1500);
+
+
+}
+
+// task1();
+// task2();
+// task3();
+// task4();
+// console.log("Task 4 done"); //All these work in order - synchronously
+
+
+task1(()=>{
+    task2(()=>{
+        task3(()=>{
+            task4(()=>{
+                console.log("all tasks done");
+            });
+        });
+    });
+});
